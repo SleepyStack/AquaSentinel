6e2d737cbc8b8259eea005c86b597da8dcb3574c
Chirag <chirag2006chopra@gmail.com>
2025-09-04 00:03:32 +0530

    Add:Backend

---
+++ Backend/AquaSentinel/src/main/java/com/infinityloop/aquasentinel/AquaSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.infinityloop.aquasentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaSentinelApplication.class, args);
+    }
+
+}
---
+++ Backend/AquaSentinel/src/test/java/com/infinityloop/aquasentinel/AquaSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinityloop.aquasentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
